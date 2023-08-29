842aaa5e9678d8b00736ba911e1998e3f1d94adc
Rafael Deluna <delunarafael@yahoo.com>
2023-08-29 03:09:03 -0400

    Completed Sprint 9 Module 1 Project

--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React from 'react';
+import Todo from './Todo';
 
 export default class TodoList extends React.Component {
+  state = {
+    showAll: true
+  }
+
+  toggleShowall = evt => {
+    this.setState({
+      ...this.state,
+      showAll: !this.state.showAll
+    })
+  }
+
+
   render() {
+    const {todos, toggleCompletion} = this.props;
+    const filtered = todos.filter(td => !td.completed || this.state.showAll)
     return (
       <div>
-        TodoList
+        {
+          filtered.map((todo) => (<Todo key={todo.id} todo={todo} toggleCompletion={toggleCompletion}/>))
+        }
+        <button onClick={this.toggleShowall}>{this.state.showAll ? 'Hide Completed Todos' : 'Show All'}</button>
       </div>
     )
   }
